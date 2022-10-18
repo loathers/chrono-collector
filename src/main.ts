1f55ac2457087955e83a760e5ec1471028cd7462
Tokoeka <nzgamer666@gmail.com>
2022-10-18 16:30:30 +0100

    prettier

--- src/main.ts
+++ src/main.ts
@@ -84,7 +84,13 @@ export function main(command?: string) {
         outfit: () =>
           chooseQuestOutfit(
             { location: quest.location, isFree: true },
-            { back: $item`protonic accelerator pack`, avoid: get("ghostLocation") === $location`Icy Peak` ? $items`Great Wolf's beastly trousers` : [] }
+            {
+              back: $item`protonic accelerator pack`,
+              avoid:
+                get("ghostLocation") === $location`The Icy Peak`
+                  ? $items`Great Wolf's beastly trousers`
+                  : [],
+            }
           ),
         completed: () => get("questPAGhost") === "unstarted",
         combat: new ChronerStrategy(() =>
