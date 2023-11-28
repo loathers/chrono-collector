4974f4a13c82dd5cdf5ae8de7bb12048c77cbbba
Alium58 <bennelson3927@gmail.com>
2023-11-28 01:47:16 -0600

    Fix getting goo skill (#93)
    * Update main.ts
    
    * Update main.ts
    
    * Update src/main.ts
    
    ---------
    
    Co-authored-by: Joe Kirchoff <Kirchoff.Joseph.P@gmail.com>

--- src/main.ts
+++ src/main.ts
@@ -80,6 +80,20 @@ export function main(command?: string) {
         sobriety: "either",
       },
       {
+        name: "Grey You Attack Skill",
+        completed: () =>
+          have($skill`Nantlers`) ||
+          have($skill`Nanoshock`) ||
+          have($skill`Audioclasm`),
+        do: $location`The Haunted Storage Room`,
+        ready: () =>
+          myClass() === $class`Grey Goo` &&
+          canAdventure($location`The Haunted Storage Room`),
+        combat: new ChronerStrategy(() => Macro.standardCombat()),
+        sobriety: "sober",
+        choices: { 886: 6 },
+      },
+      {
         name: "Clara's Bell",
         completed: () => !have($item`Clara's bell`) || get("_claraBellUsed"),
         do: () => {
@@ -295,19 +309,6 @@ export function main(command?: string) {
             .skill($skill`Spit jurassic acid`)
             .abort();
         }),
-        sobriety: "sober",
-      },
-      {
-        name: "Grey You Attack Skill",
-        completed: () =>
-          have($skill`Nantlers`) ||
-          have($skill`Nanoshock`) ||
-          have($skill`Audioclasm`),
-        do: $location`The Haunted Storage Room`,
-        ready: () =>
-          myClass() === $class`Grey Goo` &&
-          canAdventure($location`The Haunted Storage Room`),
-        combat: new ChronerStrategy(() => Macro.standardCombat()),
         sobriety: "sober",
       },
     ],
